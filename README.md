2990b957cd24cbf12b17104dccb70f60ae57a12a
Alex Colby <alexdcolby@pm.me>
2025-08-08 19:01:17 +0100

    docs: updates command format

--- README.md
+++ README.md
@@ -3,15 +3,12 @@
 NixOS templates for development shells using a consistent set of commands.\
 In all development shells the following commands are available:
 
-- `.init`: re-initialise the project
+- `.init`: initialise the project
 - `.sync`: installs all dependencies of the project
 - `.add`: adds new dependencies for the project
 - `.del`: deletes dependencies from the project
 - `.build`: build an executable version of the project
 - `.run`: runs the executable for the project
 - `.test`: tests the project code
-- `.lint`: performs static linting on the project, such as:
-  - linting
-  - formatting
-  - code coverage
-  - dependency analysis
+- `.lint`: performs static checks on the project,\
+  such as linting, formatting and dependency analyses
--- flake.nix
+++ flake.nix
@@ -5,18 +5,15 @@
     { ... }:
     let
       cmds = ''
-        - `.init`: re-initialise the project
+        - `.init`: initialise the project
         - `.sync`: installs all dependencies of the project
         - `.add`: adds new dependencies for the project
         - `.del`: deletes dependencies from the project
         - `.build`: build an executable version of the project
         - `.run`: runs the executable for the project
         - `.test`: tests the project code
-        - `.lint`: performs static linting on the project, such as:
-          - linting
-          - formatting
-          - code coverage
-          - dependency analysis
+        - `.lint`: performs static checks on the project,\
+          such as linting, formatting and dependency analyses
       '';
     in
     {
@@ -28,7 +25,8 @@
 
           Rust library development with `cargo`.
 
-          This provides the following commands:
+          Initialise the shell with `nix develop`\
+          which will provide the following commands:
 
           ${cmds}
         '';
